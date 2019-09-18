26d2ea29a9d002add97d572d8d9cd12d1a74d89d
shiyisanyecao <2327532812@qq.com>
2019-09-18 17:25:14 +0800

    leftNav

--- src/components/leftNav/leftNav.vue
+++ src/components/leftNav/leftNav.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="left-container">
-        <div class="title-nav">技术频道
+<div>
+    <div class="left-container" v-for="(i,ii) in dataList" :key="ii">
+        <div class="title-nav">{{i.name}}
         </div>
         <div class="choose-nav">
-            <div class="each-nav" :class="item.selected?'active':''" @click="clickNav(index)" v-for="(item, index) in dataList" :key="index">
+            <div class="each-nav" :class="item.selected?'active':'not-active'" @click="clickNav(ii, index)" v-for="(item, index) in i.commonList" :key="index">
                 <i class="iconfont" ref="iconClick"
                     :style="`content:url(${item.image})`"
                 ></i><span>{{item.name}}</span>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
@@ -25,16 +27,20 @@ export default {
     methods: {
         initNavData() {
             this.$http.get('/community/leftNav/list').then(res => {
-                this.dataList = res.data.data.commonList;
+                this.dataList = res.data.data;
             })
         },
-        clickNav(index) {
-            for(var i = 0; i < this.dataList.length; i ++) {
-                this.$refs.iconClick[i].style.content = `url(${this.dataList[i].image})`;
-                this.dataList[i].selected = false;
+        clickNav(ii, index) {
+            for(let i = 0; i < this.dataList.length; i ++) {
+                for(let j = 0; j < this.dataList[i].commonList.length; j ++) {
+                    this.dataList[i].commonList[j].selected = false;
+                }
             }
-            this.dataList[index].selected = true;
-            this.$refs.iconClick[index].style.content = `url(${this.dataList[index].imageHover})`;
+            for(let z = 0; z < this.dataList[ii].commonList.length; z ++) {
+                this.$refs.iconClick[z].style.content = `url(${this.dataList[ii].commonList[z].image})`;
+            }
+            this.dataList[ii].commonList[index].selected = true;
+            this.$refs.iconClick[index].style.content = `url(${this.dataList[ii].commonList[index].imageHover})`;
         }
     }
 }
--- src/mock/mock.js
+++ src/mock/mock.js
@@ -6,83 +6,105 @@ export default {
     mockData() {
         Mock.mock(/\/community\/leftNav\/list/, 'get', req => {
             return {
-                data: {
-                    commonList: [
-                        {
-                            id: 1,
-                            selected: false,
-                            image: '/static/image/qianduan.png',
-                            imageHover: '/static/image/qianduanHover.png',
-                            name: '前端'
-                        },{
-                            id: 2,
-                            selected: false,
-                            image: '/static/image/qianduan.png',
-                            imageHover: '/static/image/qianduanHover.png',
-                            name: '后端'
-                        },{
-                            id: 3,
-                            selected: false,
-                            image: '/static/image/xiaochengxu.png',
-                            imageHover: '/static/image/xiaochengxuHover.png',
-                            name: '小程序'
-                        },{
-                            id: 4,
-                            selected: false,
-                            image: '/static/image/xiaochengxu.png',
-                            imageHover: '/static/image/xiaochengxuHover.png',
-                            name: '人工智能'
-                        },{
-                            id: 5,
-                            selected: false,
-                            image: '/static/image/qukuailian.png',
-                            imageHover: '/static/image/qukuailianHover.png',
-                            name: '区块链'
-                        },{
-                            id: 6,
-                            selected: false,
-                            image: '/static/image/anquan.png',
-                            imageHover: '/static/image/anquanHover.png',
-                            name: '安全'
-                        },{
-                            id: 7,
-                            selected: false,
-                            image: '/static/image/android.png',
-                            imageHover: '/static/image/androidHover.png',
-                            name: 'Android'
-                        },{
-                            id: 8,
-                            selected: false,
-                            image: '/static/image/IOS.png',
-                            imageHover: '/static/image/iosHover.png',
-                            name: 'IOS'
-                        },{
-                            id: 9,
-                            selected: false,
-                            image: '/static/image/tool.png',
-                            imageHover: '/static/image/toolHover.png',
-                            name: '工具'
-                        },{
-                            id: 10,
-                            selected: false,
-                            image: '/static/image/ren.png',
-                            imageHover: '/static/image/renHover.png',
-                            name: '程序员'
-                        },{
-                            id: 11,
-                            selected: false,
-                            image: '/static/image/hangye.png',
-                            imageHover: '/static/image/hangyeHover.png',
-                            name: '行业'
-                        },{
-                            id: 12,
-                            selected: false,
-                            image: '/static/image/shequ.png',
-                            imageHover: '/static/image/shequHover.png',
-                            name: '官方社区'
-                        }
-                    ]
-                }
+                data: [
+                    {
+                        name: '极术社区',
+                        commonList: [
+                            {
+                                id: 1,
+                                selected: false,
+                                image: '/static/image/qianduan.png',
+                                imageHover: '/static/image/qianduanHover.png',
+                                name: '人工智能'
+                            }, {
+                                id: 2,
+                                selected: false,
+                                image: '/static/image/qianduan.png',
+                                imageHover: '/static/image/qianduanHover.png',
+                                name: '芯片软硬件'
+                            }
+                        ]
+                    },
+                    {
+                        name: '技术频道',
+                        commonList: [
+                            {
+                                id: 1,
+                                selected: false,
+                                image: '/static/image/qianduan.png',
+                                imageHover: '/static/image/qianduanHover.png',
+                                name: '前端'
+                            },{
+                                id: 2,
+                                selected: false,
+                                image: '/static/image/qianduan.png',
+                                imageHover: '/static/image/qianduanHover.png',
+                                name: '后端'
+                            },{
+                                id: 3,
+                                selected: false,
+                                image: '/static/image/xiaochengxu.png',
+                                imageHover: '/static/image/xiaochengxuHover.png',
+                                name: '小程序'
+                            },{
+                                id: 4,
+                                selected: false,
+                                image: '/static/image/xiaochengxu.png',
+                                imageHover: '/static/image/xiaochengxuHover.png',
+                                name: '人工智能'
+                            },{
+                                id: 5,
+                                selected: false,
+                                image: '/static/image/qukuailian.png',
+                                imageHover: '/static/image/qukuailianHover.png',
+                                name: '区块链'
+                            },{
+                                id: 6,
+                                selected: false,
+                                image: '/static/image/anquan.png',
+                                imageHover: '/static/image/anquanHover.png',
+                                name: '安全'
+                            },{
+                                id: 7,
+                                selected: false,
+                                image: '/static/image/android.png',
+                                imageHover: '/static/image/androidHover.png',
+                                name: 'Android'
+                            },{
+                                id: 8,
+                                selected: false,
+                                image: '/static/image/IOS.png',
+                                imageHover: '/static/image/iosHover.png',
+                                name: 'IOS'
+                            },{
+                                id: 9,
+                                selected: false,
+                                image: '/static/image/tool.png',
+                                imageHover: '/static/image/toolHover.png',
+                                name: '工具'
+                            },{
+                                id: 10,
+                                selected: false,
+                                image: '/static/image/ren.png',
+                                imageHover: '/static/image/renHover.png',
+                                name: '程序员'
+                            },{
+                                id: 11,
+                                selected: false,
+                                image: '/static/image/hangye.png',
+                                imageHover: '/static/image/hangyeHover.png',
+                                name: '行业'
+                            },{
+                                id: 12,
+                                selected: false,
+                                image: '/static/image/shequ.png',
+                                imageHover: '/static/image/shequHover.png',
+                                name: '官方社区'
+                            }
+                        ]
+                    }
+                    
+                ]
             }
         })
     }
